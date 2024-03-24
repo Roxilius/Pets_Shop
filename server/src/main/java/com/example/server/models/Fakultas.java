d431abf0e33aae9bbd3ef48d5ebd21637d4fc3ed
Your Name <you@example.com>
2024-03-24 16:12:16 +0700

    .

--- server/src/main/java/com/example/server/models/Fakultas.java
+++ server/src/main/java/com/example/server/models/Fakultas.java
@@ -1,5 +1,21 @@
 package com.example.server.models;
 
+import org.hibernate.annotations.UuidGenerator;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Fakultas {
-    
+    @Id
+    @UuidGenerator
+    @Column(name ="id", length = 36, nullable=false)
+    private String id;
 }
